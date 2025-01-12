d8691857d1e55f017feb2dcdc50f1f6be9ddbb80
adarshchhetri9 <chhetriadarsh912@gmail.com>
2025-01-12 17:09:20 +0530

    new changes

--- myLinuxConfig/main/configForNixos.nix
+++ myLinuxConfig/main/configForNixos.nix
@@ -2,7 +2,11 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, inputs,  ... }:
+
+let
+	vimConfig = import ./nvf-configuration.nix { inherit pkgs; };
+in
 
 {
   imports =
@@ -13,14 +17,6 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  # Screen lock enabling with Hyprlock
-  programs.hyprlock.enable = true;
-  security.pam.services.hyprlock = {};
-
-  #UWSM
-  #programs.hyprland.withUWSM = true; #some error on this  
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
@@ -31,16 +27,6 @@
 
   # Enable networking
   networking.networkmanager.enable = true;
-
-  # For screen Brightness setting
-  programs.light.enable = true;
-  services.actkbd = {
-    enable = true;
-    bindings = [
-      { keys = [ 224 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -A 10"; }
-      { keys = [ 225 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -U 10"; }
-    ];
-  };
 
   # Set your time zone.
   time.timeZone = "Asia/Kolkata";
@@ -66,63 +52,57 @@
     variant = "";
   };
 
-  # Display manager enabling
-  services.xserver.enable = true;
-
-  # For hyprland
-  programs.hyprland.enable = true;	
-  # programs.hyprland.xwayland.enable = true;
-  
-
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.art = {
     isNormalUser = true;
-    description = "Adarsh";
+    description = "Art";
+    shell = pkgs.fish;
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [];
   };
 
+  # Enable automatic login for the user.
+  services.getty.autologinUser = "art";
+
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # Making fish my default shell
-  users.users.art.shell = pkgs.fish;
-  programs.fish.enable = true; 
-  users.users.art.useDefaultShell = true;
-
   # List packages installed in system profile. To search, run:
-  fonts.packages = with pkgs; [
-  	nerdfonts
-  	];
-
   # $ nix search wget
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
-	kitty
-	brave
-	fastfetch
+	menulibre
+	kitty 
+	fish 
+	neofetch 
+	hyprlock 
+	rofi-wayland-unwrapped  
+	brave 
 	nemo-with-extensions
-	swayfx-unwrapped
-	rofi-wayland-unwrapped
-	waybar	
-	git
-	pipewire
-	wireplumber
-	xdg-desktop-portal-hyprland
-	mesa
-	hyprlock
-	vscode
-	dotfiles
-	dunst
-	hyprpaper
-	line-awesome
-	hyprshot
+	#ntfs3g 
 	stow
-	#swaynotificationcenter #not working need to config
-  ];
+	git
+	xdg-desktop-portal-hyprland
+	starship
+	hyprshot
+	telegram-desktop
+	hyprpanel
+	ags
+	upower
+	inputs.zen-browser.packages.x86_64-linux.default #zenBrowser
+	btop
+	cmatrix
+	vscode
 
-  # Some programs need SUID wrappers, can be configured further or are
+	#For discord
+	(discord.override {
+      	#withOpenASAR = true; # can do this here too
+     	withVencord = true;
+    	})
+]; 
+
+# Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
   # programs.gnupg.agent = {
@@ -133,7 +113,7 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  # services.openssh.enable = true
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
@@ -149,4 +129,62 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.11"; # Did you read the comment?
 
+	# Enabling lightdm
+	services.xserver.enable = true;
+	services.displayManager.autoLogin.enable = true; #autoLogin
+	services.displayManager.autoLogin.user = "art";
+
+	# Enabling Hyprland
+	programs.hyprland.enable = true;
+
+	# Enabling fish as my default shell
+	programs.fish.enable = true;
+	users.defaultUserShell = pkgs.fish;
+
+	# Enabling Fonts & Icons
+	fonts.packages = with pkgs; [
+	  nerd-fonts.fira-code
+	  nerd-fonts.droid-sans-mono
+	];
+
+	# Enabling Flakes for experimental programs
+	nix.settings.experimental-features = ["nix-command" "flakes"]; 
+	
+	# Enabling partition Disks in for both Linux and Windows
+	fileSystems."/mnt/nvme0n1p5" = {
+	  device = "/dev/nvme0n1p5";
+	  fsType = "ntfs-3g";
+	};
+
+	# Brightness key mapping
+	programs.light.enable = true;
+  	services.actkbd = {
+    	enable = true;
+    	bindings = [
+      	{ keys = [ 224 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -U 5"; }
+      	{ keys = [ 225 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -A 5"; }
+    			];
+  		};
+
+	# Dependency for battery
+ 	services.upower.enable = true;
+	
+	# Neovim in Nix (NVF)
+	 programs.nvf = vimConfig.programs.nvf;
+	#	enable = true;
+	#	settings = {
+	#		vim = {
+	#			theme = {
+	#			enable = true; 
+	#			name = "gruvbox";
+	#			style = "dark";
+	#				};
+	#		languages.nix.enable = true;
+	#		statusline.lualine.enable = true;
+	#		telescope.enable = true;
+	#		autocomplete.nvim-cmp.enable = true;
+	#			};};
+	#		};
+
+ 
 }
